f2ae80b05c8f306f1841b18edb3d5f50a8d7ba5d
PROMNY <p.promny@gmail.com>
2019-03-23 15:48:38 +0100

    comlexity decrease

--- pymc/lattice.py
+++ pymc/lattice.py
@@ -12,13 +12,20 @@ class lattice:
         self.n = n
         self.lattice_type = lattice_type
         self.is_periodic = is_periodic
+
         assert (lattice_type in ["square", "graphene"]), "Unknown lattice type"
 
-        self.fill_adj_matrix()
+        if self.lattice_type == "graphen":
+            assert (n % 2 == 0), "for graphen lattice n must be even"
+            assert (n >= 4), "for graphen lattice n grater or equal 4"
+            self.fill_adj_matrix_graphene()
+            self.fill_pos_matrix_graphene()
 
-        self.fill_position_matrix()
+        elif self.lattice_type == "graphen":
+            self.fill_adj_matrix_square()
+            self.fill_pos_matrix_square()
 
-    def fill_adj_matrix(self):
+    def fill_adj_matrix_square(self):
         n = self.n
         self.adj_matrix = np.zeros((n**2, n**2))
 
@@ -31,79 +38,90 @@ class lattice:
                 self.adj_matrix[i][-n+i] = 1
                 self.adj_matrix[-n+i][i] = 1
 
-        if self.lattice_type == "square":
-            for i in range(n**2):
-                if i % n > 0:
-                    self.adj_matrix[i][i-1] = 1
-                    self.adj_matrix[i-1][i] = 1
+        for i in range(n**2):
+            if i % n > 0:
+                self.adj_matrix[i][i-1] = 1
+                self.adj_matrix[i-1][i] = 1
 
-            if self.is_periodic:
-                for i in range(n):
-                    self.adj_matrix[i*n][i*n + n - 1] = 1
-                    self.adj_matrix[i*n + n - 1][i*n] = 1
+        if self.is_periodic:
+            for i in range(n):
+                self.adj_matrix[i*n][i*n + n - 1] = 1
+                self.adj_matrix[i*n + n - 1][i*n] = 1
 
-        else:
-            assert (n % 2 == 0), "for graphen lattice n must be even"
-            assert (n >= 4), "for graphen lattice n grater or equal 4"
-
-            R = [2*i for i in range(n//2)]
-            R += [2*i+1+n for i in range(n//2)][:-1]
-
-            for i in range(n**2):
-                j = i % (2*n)
-                if j in R:
-                    self.adj_matrix[i][i+1] = 1
-                    self.adj_matrix[i+1][i] = 1
-
-            if self.is_periodic:
-                for i in range(1, n-1):
-                    self.adj_matrix[n*i][n*i + n - 1] = 1
-                    self.adj_matrix[n*i + n - 1][n*i] = 1
-
-    def fill_position_matrix(self):
+    def fill_adj_matrix_graphene(self):
         n = self.n
-        self.position_matrix = []
+        self.adj_matrix = np.zeros((n**2, n**2))
 
-        if self.lattice_type == "square":
-            for i in range(n**2):
-                x = i % n
-                y = n - (i // n) - 1
-                self.position_matrix.append([x, y])
-        else:
-            for i in range(n**2):
-                j = i % n
-                if i % (2 * n) < n:
-                    if i % 2 == 0:
-                        x = 0.5 + 1.5 * j
-                    else:
-                        x = 1.5 * j
+        for i in range(n**2 - n):
+            self.adj_matrix[i][i+n] = 1
+            self.adj_matrix[i+n][i] = 1
+
+        if self.is_periodic:
+            for i in range(n):
+                self.adj_matrix[i][-n+i] = 1
+                self.adj_matrix[-n+i][i] = 1
+
+        R = [2*i for i in range(n//2)]
+        R += [2*i+1+n for i in range(n//2)][:-1]
+
+        for i in range(n**2):
+            j = i % (2*n)
+            if j in R:
+                self.adj_matrix[i][i+1] = 1
+                self.adj_matrix[i+1][i] = 1
+
+        if self.is_periodic:
+            for i in range(1, n-1):
+                self.adj_matrix[n*i][n*i + n - 1] = 1
+                self.adj_matrix[n*i + n - 1][n*i] = 1
+
+    def fill_pos_matrix_square(self):
+        n = self.n
+        self.pos_matrix = []
+
+        for i in range(n**2):
+            x = i % n
+            y = n - (i // n) - 1
+            self.pos_matrix.append([x, y])
+
+        self.pos_matrix = np.asarray(self.pos_matrix)
+
+    def fill_pos_matrix_graphene(self):
+        n = self.n
+        for i in range(n**2):
+            j = i % n
+            if i % (2 * n) < n:
+                if i % 2 == 0:
+                    x = 0.5 + 1.5 * j
                 else:
-                    if i % 2 == 0:
-                        x = 1.5 * j
-                    else:
-                        x = 0.5 + 1.5 * j
-                y = np.sqrt(3)/2 * (n - (i // n) - 1)
-                self.position_matrix.append([x, y])
+                    x = 1.5 * j
+            else:
+                if i % 2 == 0:
+                    x = 1.5 * j
+                else:
+                    x = 0.5 + 1.5 * j
+            y = np.sqrt(3)/2 * (n - (i // n) - 1)
+            self.pos_matrix.append([x, y])
 
-        self.position_matrix = np.asarray(self.position_matrix)
+        self.pos_matrix = np.asarray(self.pos_matrix)
 
     def plot(self):
         plt.clf()
         n = self.n
-        X = [pos[0] for pos in self.position_matrix]
-        Y = [pos[1] for pos in self.position_matrix]
+        X = [pos[0] for pos in self.pos_matrix]
+        Y = [pos[1] for pos in self.pos_matrix]
         plt.scatter(X, Y)
 
         for i in range(n**2):
             for j in range(i, n**2):
                 if self.adj_matrix[i][j] == 1:
-                    v = self.position_matrix[i] - self.position_matrix[j]
+                    v = self.pos_matrix[i] - self.pos_matrix[j]
                     len = np.sqrt(v[0]**2 + v[1]**2)
 
-                    x = [self.position_matrix[i][0],
-                         self.position_matrix[j][0]]
-                    y = [self.position_matrix[i][1],
-                         self.position_matrix[j][1]]
+                    x = [self.pos_matrix[i][0],
+                         self.pos_matrix[j][0]]
+                    y = [self.pos_matrix[i][1],
+                         self.pos_matrix[j][1]]
 
                     if len < 1.1:
                         plt.plot(x, y, color="black")
